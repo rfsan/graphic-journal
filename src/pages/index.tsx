7996139fd8329989b74c9bf82410ae9f03ddcc92
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2022-08-18 18:10:46 -0500

    New post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,14 @@ import { Header } from "@/components";
 export default function Home() {
   const cms = [
     {
-      date: "Sunday, August 14, 2022",
-      title: "Flowers",
-      tags: "Little things",
+      date: "Tuesday, August 16, 2022",
+      title: "Focus",
+      tags: "Entrepreneurship",
       description:
-        "Lately I have started buying flowers for Paula and for the house. My favorite moment is when they open the day after they have been put in water.",
-      color: "text-[#75c448]",
+        "I decided to leave the project I've been working on for the last 8 months to focus on offering web development services as a freelancer. My brother made me realize that we were building a solution to a problem that neither we nor anyone close to us has.",
+      color: "text-[#FFC86B]",
       image: {
-        src: "/images/flower.png",
+        src: "/images/bye-trazo.png",
         alt: "A sad flower because the sun is setting",
         width: 1024,
         height: 1024,
