cfdfa885bcd8abead77439e66a38c5e08502b01a
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2022-08-18 18:46:11 -0500

    Add hover information to images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header className="fixed z-10 w-full bg-[#0F0F0F]">
-      <div className="max-w-screen-xl px-4 py-2 lg:mx-auto lg:px-8">
+      <div className="flex max-w-screen-xl justify-between px-4 py-2 lg:mx-auto  lg:px-8">
         <h1 className="font-serif leading-5 text-[#DADADA]">
           rafael <br />
           <span className="font-bold text-[#ffffff]">
@@ -9,6 +9,10 @@ export const Header = () => {
             journal
           </span>
         </h1>
+        <span className="mt-1 text-right text-sm italic text-[#A4A4A4]">
+          click the images
+          <br /> for more details
+        </span>
       </div>
     </header>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,22 @@ export default function Home() {
     {
       date: "Tuesday, August 16, 2022",
       title: "Focus",
-      tags: "Entrepreneurship",
+      tags: "Juan, Entrepreneurship",
       description:
-        "I decided to leave the project I've been working on for the last 8 months to focus on offering web development services as a freelancer. My brother made me realize that we were building a solution to a problem that neither we nor anyone close to us has.",
+        "I decided to leave the project I've been working on for the last 8 months to focus on offering web development services as a freelancer. Juan made me realize that we were building a solution to a problem that neither we nor anyone close to us has.",
       color: "text-[#FFC86B]",
       image: {
         src: "/images/bye-trazo.png",
         alt: "A sad flower because the sun is setting",
         width: 1024,
         height: 1024,
+      },
+      creation: {
+        model: "DALL-E 2",
+        prompt:
+          "A cute baby snake opening a new shoe store, the snake is happy and has a smile in his face, the background has a lot of sneakers, high detail, 3D digital art",
+        generations: "5",
+        cost: "$0.65 USD",
       },
     },
     {
@@ -29,6 +36,13 @@ export default function Home() {
         alt: "A couple kissing while watching Succession.",
         width: 1024,
         height: 1024,
+      },
+      creation: {
+        model: "DALL-E 2",
+        prompt:
+          "Soviet propaganda featuring a woman and a man kissing, while a businessman yells on television",
+        generations: "10",
+        cost: "$1.30 USD",
       },
     },
   ];
@@ -54,6 +68,16 @@ export default function Home() {
                   layout="responsive"
                   alt="DALL-E Image"
                 />
+                <div className="absolute top-0 left-0 flex h-full w-full flex-col gap-y-4 bg-[#0F0F0F]/80 p-2 text-sm opacity-0 hover:opacity-100 lg:p-6 lg:text-base">
+                  {Object.entries(post.creation).map(([key, value]) => (
+                    <div key={key} className="flex flex-row gap-x-3">
+                      <span className={`font-bold capitalize ${post.color}`}>
+                        {key}
+                      </span>
+                      <span>{value} </span>
+                    </div>
+                  ))}
+                </div>
                 {/* <span className="mt-1 text-right text-sm italic text-[#A4A4A4]">
                   click image for details
                 </span> */}
