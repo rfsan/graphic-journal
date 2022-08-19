8adcec80c2c55ce43a68ff7daa14cf6875f93108
Rafael Sanabria <rafael.d.sanabria@gmail.com>
2022-08-18 19:29:15 -0500

    Test of the meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Graphic Journal</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧙‍♂️</text></svg>"
+        ></link>
+        <meta
+          name="description"
+          content="Rafael's Graphic Journal. I use Text-to-Image models to create an image for my daily entries."
+        />
+        <meta property="og:title" content="Rafael's Graphic Journal" />
+        <meta property="og:url" content="https://graphic-journal.vercel.app/" />
+        <meta
+          property="og:description"
+          content="description of your website/webpage"
+        />
+        <meta
+          property="og:image"
+          content="https://mywebsite.com/images/fuckoff.png"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
